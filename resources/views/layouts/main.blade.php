1438f9ec2edbdd6c1100fec9de4f3cda3a26ee7d
binu <binu@sesametechnologies.in>
2023-05-28 20:12:37 +0530

    updated

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -94,39 +94,9 @@
 
 
 
-    <div class="bottommenu">
-        <div class="bottomlogo">
-            <span class="dotlogo">&bullet;</span>
-            <img class="ll" src="{{ asset('asset/image/logo.jpg') }}" alt="logo1" />
-            <span class="dotlogo">&bullet;</span>
-        </div>
-        <ul class="nav nav-tabs bottomlinks">
-            <li role="presentation"><a href="#/" role="button">ABOUT US</a></li>
-            <li role="presentation"><a href="#/">CATEGORIES</a></li>
-            <li role="presentation"><a href="#/">PREORDERS</a></li>
-            <li role="presentation"><a href="#/">CONTACT US</a></li>
-            <li role="presentation"><a href="#/">RECEIVE OUR NEWSLETTER</a></li>
-        </ul>
-        <p>Pre-Owned Used Cars</p>
-        <img src="{{ asset('asset/image/line.png') }}" alt="line" /> <br />
-        <div class="bottomsocial">
-            <a href="#"><i class="fa fa-facebook"></i></a>
-            <a href="#"><i class="fa fa-twitter"></i></a>
-            <a href="#"><i class="fa fa-google-plus"></i></a>
-            <a href="#"><i class="fa fa-pinterest"></i></a>
-        </div>
-        <div class="footer">
-            <div class="copyright">
-                &copy; Copy right {{ date('Y') }} | <a href="#">Privacy </a>|
-                <a href="#">Policy</a>
-            </div>
-            <div class="atisda">
-                Designed by
-                <a href="http://www.webdomus.net/">Web Domus Italia - Web Agency
-                </a>
-            </div>
-        </div>
-    </div>
+
+
+  
 
     <script type="text/javascript" src="{{ asset('asset/source/bootstrap-3.3.6-dist/js/jquery.js') }}"></script>
     <script type="text/javascript" src="{{ asset('asset/source/js/isotope.js') }}"></script>
